f77323bafc67e1392b89d8d4f36346676c939bf7
quigt7936 <71140817+quigt7936@users.noreply.github.com>
2020-10-07 09:51:53 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,15 +1,29 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * Adds all the odd numbers up to a given number
+ * @author Thomas Quigley
  */
 public class Main {
-
-  /**
-   * The method that is executed when you hit the run button.
-   * @param args the command line arguments
-   */
   public static void main(String[] args) {
-    // TODO code application logic here
+    Scanner input = new Scanner(System.in);
     
+    // ask for the number
+    System.out.println("Please enter the maximum number to sum to:");
+    int userNumber = input.nextInt();
+
+    // initiliaze the sum variable 
+    int sum = 0;
+
+    // for loop that stops when it reaches the users number
+    for (int i = 0; i <= userNumber; i++) {
+      // initiliaze the variable that tells the code if the number is odd or even
+      int oddEven = i % 2;
+      // if the number is odd add to sum if even go onto the next number
+      if (oddEven == 1) {
+        sum = sum + i;
+      }
+    }
+    // tell the user the sum
+    System.out.println("The sum of the odd numbers is " + sum);
   }
 }
